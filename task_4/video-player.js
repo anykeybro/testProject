7e16c4acea1a815b54263c43ec4de86a69c02513
Maksim <mjs-studio@inbox.ru>
2015-07-17 17:59:56 +0300

    commit task №4.  custom video player.

---
+++ task_4/video-player.js
@@ -0,0 +1,61 @@
+/**
+ * Player main proto definition
+ * @param node
+ * @constructor
+ */
+var Player;
+Player = function (node) {
+    //this = {}
+    this._node = node;
+    this.playButton = document.querySelector('.play-pause');
+    this.playButton.addEventListener('click', this.playPause.bind(node));
+
+    this.rewindBackwardButton = document.querySelector('.rewind-backward');
+    this.rewindBackwardButton.addEventListener('click', this.rewindBackward.bind(node));
+
+    this.rewindForwardButton = document.querySelector('.rewind-forward');
+    this.rewindForwardButton.addEventListener('click', this.rewindForward.bind(node));
+
+};
+
+/**
+ * play or pause video
+ */
+Player.prototype.playPause = function(nodeBtnPlay){
+    var btn =nodeBtnPlay.target;
+    if(this.paused){
+        this.play();
+        console.log('played');
+        btn.style.background="url('images/pause.png')";
+
+    }else{
+        this.pause();
+        console.log('pause');
+        btn.style.background="url('images/play.png')";
+    }
+    console.log(btn.style.background);
+};
+
+/**
+ * rewind backward
+*/
+Player.prototype.rewindBackward = function() {
+    this.currentTime-=3;
+};
+
+/**
+ * rewind forward
+ */
+Player.prototype.rewindForward = function (){
+    this.currentTime+=3;
+};
+
+
+videoNode = document.querySelector('#video');
+var myPlayer = new Player( videoNode );
+
+
+
+
+
+
